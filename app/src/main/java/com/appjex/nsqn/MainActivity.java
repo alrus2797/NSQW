58bd5f540cadaad0c86de61bb77ea6211ed37e8b
alrus2797 <alrus2797@gmail.com>
2017-12-04 22:43:12 -0500

    Gradle and database example

--- app/src/main/java/com/appjex/nsqn/MainActivity.java
+++ app/src/main/java/com/appjex/nsqn/MainActivity.java
@@ -2,12 +2,58 @@ package com.appjex.nsqn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonS;
+    Button buttonN;
+    TextView text;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("clima");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonN=(Button)findViewById(R.id.buttonNublado);
+        buttonS=(Button)findViewById(R.id.buttonSoleado);
+        text=(TextView)findViewById(R.id.textt);
+
+        buttonS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myRef.setValue("Soleado");
+            }
+        });
+        buttonN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myRef.setValue("Nublado");
+            }
+        });
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                text.setText(dataSnapshot.getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 }
